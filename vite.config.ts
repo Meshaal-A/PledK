36dbb98fe5dac8123b8d37032fda98c77144690d
origin <mishalayubkhan10@gmail.com>
2024-12-06 20:31:58 +0530

    fix(config): Resolve merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,3 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 5804884 (feat(header):Added Navbar)
-=======
-
->>>>>>> 1306d36 (chore(deps): update @vitejs/plugin-react to version 4.3.4)
